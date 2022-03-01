72f6fe3f5134c41e3410009f5b66fedc8df58d96
Anish Nandhan <anishnandhan03@gmail.com>
2022-03-01 10:52:13 +0530

    added initial tk elements

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter
+import os
+
+root = tkinter.Tk()
+
+input = tkinter.Entry()
+input.pack()
+
+browse = tkinter.Button()
+browse.pack()
+
+submit = tkinter.Button()
+submit.pack()
+
+root.mainloop()
