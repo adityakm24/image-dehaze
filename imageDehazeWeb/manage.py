84ddbba55eecf73962628976f04c8ede9c208f95
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2022-03-17 13:12:49 +0530

    file upadates

--- imageDehazeWeb/manage.py
+++ imageDehazeWeb/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imageDehaze.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ImageDehazeWeb.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
