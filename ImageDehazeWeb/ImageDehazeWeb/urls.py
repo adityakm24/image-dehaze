04fe88da07690cba66fc5c51552ef318fe698065
AnishNandhan <anishnandhan03@gmail.com>
2022-03-18 08:54:55 +0530

    added initial HTML elements

--- ImageDehazeWeb/ImageDehazeWeb/urls.py
+++ ImageDehazeWeb/ImageDehazeWeb/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from openimage.views import homeView
+
 urlpatterns = [
+    path('', homeView, name='home'),
     path('admin/', admin.site.urls),
 ]
