d5b4d6957676dd6a5e4df5c370651d864df617f5
Aditya Krishnan Mohan <38864621+adityakm24@users.noreply.github.com>
2022-03-20 21:00:42 +0530

    Delete ImageDehazeWeb directory

--- ImageDehazeWeb/openimage/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpenimageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'openimage'
--- ImageDehazeWeb/openimage/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def homeView(request, *args, **kwargs):
-    return render(request, "home.html", {})
